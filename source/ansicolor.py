ad4f4b640fbf2de34d01e745ab2608a220d98158
Sungju Kwon <sungju@Sungjus-MBP-M3-16.local>
2024-07-03 08:17:03 +1000

    Changes in head comment

--- source/ansicolor.py
+++ source/ansicolor.py
@@ -17,6 +17,7 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
+
 import sys
 import os
 
